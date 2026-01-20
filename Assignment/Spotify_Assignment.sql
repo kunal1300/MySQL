1dd771a6dcb864ec28fe2fb44c6c210926e66d8b
kunal1300 <46293411+kunal1300@users.noreply.github.com>
2026-01-20 10:48:50 +0530

    update

---
+++ Assignment/Spotify_Assignment.sql
@@ -0,0 +1,76 @@
+CREATE DATABASE SPOTIFY_PROJECT;
+
+use SPOTIFY_PROJECT;
+select * from spotify_songs_2024;
+
+--1.Songs with more than 500 million Spotify streams
+UPDATE spotify_songs_2024
+SET Spotify_Streams = 0
+WHERE Spotify_Streams IS NULL;
+
+ALTER TABLE spotify_songs_2024
+ALTER COLUMN Spotify_Streams float;
+
+
+
+select count(Spotify_Streams)as "Songs more than 500 Million"  from spotify_songs_2024 where Spotify_Streams >=500000000 ;
+--output 1404
+
+--2.Tracks that are not explicit
+
+select Track from spotify_songs_2024;
+
+SELECT COUNT(Track) as "Total Tracks" ,count(distinct(Track)) as "Not Explicit"
+FROM spotify_songs_2024
+WHERE LEFT(Track, 3) LIKE '[A-Za-z1-9][A-Za-z1-9][A-Za-z1-9]';
+
+
+
+--3.Tracks with Track Score between 400 and 700
+
+
+ALTER TABLE spotify_songs_2024
+ALTER COLUMN Track_Score int;
+
+select * from spotify_songs_2024 where Track_Score between 400 and 700;
+
+
+--4.Tracks containing the word “Love” in their name
+
+
+
+select Track from spotify_songs_2024 where Track like 'love%' or Track like '%love%' or Track like '%love';
+
+
+
+--5.Songs by 'Drake', 'Adele', or 'Beyoncé'
+
+
+select count(*) as Data from spotify_songs_2024 where Artist like 'Drake' or Artist like 'Adele' or Artist like 'Beyonc%';
+
+
+--6.Find all tracks that have more than 10 million TikTok posts but less than 300 million Spotify streams.
+
+
+select count(Spotify_Streams) as "more than 10 million and less than 300 million  TikTok Posts"   from spotify_songs_2024 where Spotify_Streams > 10000000  and Spotify_Streams < 300000000 ;
+
+--7.Tracks with available YouTube Likes
+
+select COUNT(*) as "Data" from spotify_songs_2024 where YouTube_Likes is not null;
+
+--8.Tracks released in the year 2023
+
+
+select count(Track) as "released in the year 2023" from spotify_songs_2024 where Release_Date like '%2023%';
+
+--9.Tracks with more than 300M Spotify streams AND explicit
+
+
+select COUNT(*)as Data from spotify_songs_2024 where Spotify_Streams > 300000000 and Spotify_Streams is not null;
+
+
+
+--10.Tracks not by 'Taylor Swift' or 'Ed Sheeran', containing the word “night”
+
+
+select COUNT(*) as "Data" from spotify_songs_2024 where Artist not in ('Taylor Swift','Ed Sheeran') or Track like 'night';
---
+++ DDl_Dml.sql
@@ -0,0 +1,178 @@
+create database admission;
+use admission;
+
+create table Students
+(
+sid int,
+sname char(30),
+age int,
+course char(30)
+);
+
+show tables;
+describe Students;
+
+
+
+insert into Students values
+(1,"Amit",25,"mySql"),
+(2,"Neha",24,"mySql"),
+(3,"Soham",27,"mySql"),
+(4,"Snehal",28,"mySql");
+
+select * from Students;
+
+
+
+create table patients(
+pid int,
+pname varchar(255),
+dob date,
+toe datetime
+);
+
+show tables;
+
+desc patients;
+
+
+insert into patients values
+(1,'Harry','2000-05-26','2023-04-04 08:30:00'),
+(2,'potter','2000-04-27','2023-04-04 08:30:00'),
+(3,'jim','2000-05-26','2023-04-04 08:30:00'),
+(4,'tom','2000-05-26','2023-04-04 08:30:00');
+
+
+select * from patients;
+
+create table if not exists  patients(
+pid int,
+pname varchar(255),
+dob date,
+toe datetime
+);
+
+create table test(id int);
+show table status;
+show tables;
+
+drop table test;
+show tables;
+
+select * from Students;
+describe Students;
+
+alter table Students
+add column marks int default 0;
+
+alter table Students
+drop column marks;
+
+alter table Students change column sid std_id  int;
+
+select * from Students;
+
+alter table Students
+drop column age ;
+
+insert into Students (std_id,sname,course,marks) values (5,"john",null,30);
+
+insert into students (std_id,sname,marks) values (6,'Jim',50); -- Type 2 Insert
+select * from students;
+
+insert into students (std_id,sname,course) values (7,'Tim','Mysql');
+select * from students;
+
+-- adding multiple records
+insert into students values (8,'Potter','Mysql',78),(9,'Pat','Mysql',77),(10,'Albert','Mysql',88);
+select * from students;
+
+update Students
+set sname = "Natasha"
+where std_id =10;
+
+set sql_safe_updates =0;
+
+select * from Students;
+
+update Students
+set marks = null
+where marks = 0;
+
+
+update Students 
+set course ="Da",marks=85
+where std_id=9;
+
+
+update Students set Course = "mysql";
+select * from Students;
+
+
+delete from Students
+where std_id = 2;
+select * from Students;
+
+delete from Students
+where marks is null;
+
+select * from Students;
+
+delete from Students;
+
+insert into Students(std_id,sname,course,marks) values
+(1,"Ritesh","Mysql",70),
+(2,"Natasha","Mysql",90),
+(3,"saloni","Mysql",95);
+
+show create table Students;
+truncate  Students;
+
+show tables;
+
+select * from patients;
+
+select * from Students;
+
+insert into Students values (1,"tom","sql",50);
+
+insert into Students values (1,"tom","sql",50);
+
+drop table Students;
+
+create table  students
+(
+	sid integer unique,
+	sname varchar(30) not null,
+    age integer check(age>18),
+    course varchar(20)
+);
+
+select * from students;
+describe students;
+insert into Students values (1,"tom",50,"sql");
+
+
+insert into Students values (2,"tom",50,"sql");
+
+insert into Students values (null,"jon",22,"Analytics");
+insert into Students values (null,"jim",32,"mysql");
+
+insert into Students values (5,"jim",17,"mysql");
+
+
+insert into Students values (6,null,17,"mysql");
+
+insert into Students(sid,age,course) values (7,23,"python");
+
+alter table students
+modify column sname varchar(30) not null default "Smith";
+
+describe students;
+
+select * from students;
+
+insert into Students(sid,age,course) values (7,23,"python");
+
+
+
